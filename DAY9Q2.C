c768a66d13e5de550eb98e9b4af028cc759e349d
Sarthak Singh <yadavsarthak111@gmail.com>
2026-02-10 23:57:32 +0530

    Add files via upload

---
+++ DAY9Q2.C
@@ -0,0 +1,16 @@
+#include <stdbool.h>
+#include <string.h>
+
+bool isPalindrome(char* s) {
+    int left = 0;
+    int right = strlen(s) - 1;
+
+    while (left < right) {
+        if (s[left] != s[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
+}
