7511dc736048a2dd08622f516e3da7a7c2aa2742
Sarthak Singh <yadavsarthak111@gmail.com>
2026-02-12 23:54:48 +0530

    Add files via upload

---
+++ day11q2.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+    int A[10][10], B[10][10], sum[10][10];
+
+    // Input matrix dimensions
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &rows, &cols);
+
+    // Input first matrix
+    printf("Enter elements of first matrix:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &A[i][j]);
+        }
+    }
+
+    // Input second matrix
+    printf("Enter elements of second matrix:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &B[i][j]);
+        }
+    }
+
+    // Add the two matrices
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            sum[i][j] = A[i][j] + B[i][j];
+        }
+    }
+
+    // Display the result
+    printf("Sum of the two matrices:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d ", sum[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
